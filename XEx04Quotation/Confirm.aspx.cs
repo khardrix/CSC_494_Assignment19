e089fac90bbf85aed1b4867bcad506dbde891d24
RyanHuffman <huffmanr@mymail.nku.edu>
2020-04-14 23:33:37 -0400

    Add project files.

---
+++ XEx04Quotation/Confirm.aspx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace XEx04Quotation
+{
+    public partial class Confirm : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            UnobtrusiveValidationMode = UnobtrusiveValidationMode.None;
+
+            if (PreviousPage != null)
+            {
+
+                
+
+
+
+                /*
+                Response.Write(Session["salesPrice"].ToString());
+                */
+                /*
+                System.Collections.SortedList salesPrice = (System.Collections.SortedList)Session["salesPrice"];
+                System.Collections.SortedList discountAmount = (System.Collections.SortedList)Session["discountAmount"];
+                System.Collections.SortedList totalPrice = (System.Collections.SortedList)Session["totalPrice"];
+
+                lblSalesPrice.Text = salesPrice.ToString();
+                lblDiscountAmount.Text = discountAmount.ToString();
+                lblTotalPrice.Text = totalPrice.ToString();
+                */
+                // HttpContext.Current.Session[salesPrice];
+            }
+
+            /*
+             if (PreviousPage != null)
+            {
+                TextBox txtSalesPrice = (TextBox)PreviousPage.FindControl("txtSalesPrice");
+                lblSalesPrice.Text = txtSalesPrice.Text;
+
+                TextBox txtDiscountAmount = (TextBox)PreviousPage.FindControl("txDiscountAmount");
+                lblDiscountAmount.Text = txtDiscountAmount.Text;
+
+                TextBox txtTotalPrice = (TextBox)PreviousPage.FindControl("txtTotalPrice");
+                lblTotalPrice.Text = txtTotalPrice.Text;
+            }
+            */
+        }
+    }
+}
---
+++ XEx04Quotation/Default.aspx.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace XEx04Quotation
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            UnobtrusiveValidationMode = UnobtrusiveValidationMode.None;
+        }
+
+        protected void btnCalculate_Click(object sender, EventArgs e)
+        {
+            if (IsValid)
+            {
+                decimal salesPrice = Convert.ToDecimal(txtSalesPrice.Text);
+                decimal discountPercent = Convert.ToDecimal(txtDiscountPercent.Text) / 100;
+
+                decimal discountAmount = salesPrice * discountPercent;
+                decimal totalPrice = salesPrice - discountAmount;
+
+                lblDiscountAmount.Text = discountAmount.ToString("c");
+                lblTotalPrice.Text = totalPrice.ToString("c");
+
+                
+                Session.Add("salesPrice", salesPrice);
+                Session.Add("discountAmount", discountAmount);
+                Session.Add("totalPrice", totalPrice);
+
+
+                /*
+                Session["salesPrice"] = salesPrice;
+                Session["discountAmount"] = discountAmount;
+                Session["totalPrice"] = totalPrice;
+                */
+                /*
+                Response.Redirect("Confirm.aspx");
+                */
+                /*
+                Session.Add("txtSalesPrice", txtSalesPrice);
+                Session.Add("lblDiscountAmount", lblDiscountAmount);
+                Session.Add("lblTotalPrice", lblTotalPrice);
+                */
+            }
+        }
+
+        protected void btnConfirm_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("Confirm.aspx");
+        }
+    }
+}
